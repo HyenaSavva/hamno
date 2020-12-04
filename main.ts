ec59fd16d468139bfd3ebbf7d6ab33bd4da0a624
HyenaSavva <re4meister@gmai.com>
2020-12-05 01:14:45 +0200

    some fix

---
+++ main.ts
@@ -0,0 +1,21 @@
+// class Main {
+
+//     protected search: HTMLElement = document.querySelector(".search input");
+//     constructor() {
+//         this.search.addEventListener("click", () => {
+//             console.log("click");
+            
+//             this.changePosition("left", "center left");
+//         })
+//         this.search.addEventListener("blur", () => {
+//             this.changePosition("center", "center left 78px");
+//         })
+//     }
+//     private changePosition(align: string, backgroundaling: string): void {
+//         this.search.style.textAlign = align;
+//         this.search.style.backgroundPosition = backgroundaling;
+//     }
+// }
+// document.addEventListener("DOMContentLoaded", () => {
+//     new Main;
+// })
